ebdeb2af349aa2d9992957ff5c6360249d939a02
Drew Finstrom & jordan <student@epicodus.com>
2015-06-09 11:00:23 -0700

    add displaying tweets

---
+++ store.js
@@ -0,0 +1,6 @@
+var tweets = [
+  {id: '1', text: 'here is my first tweet' },
+  {id: '2', text: 'abother message from me' },
+  {id: '3', text: 'we are at Blue Star' },
+  {id: '4', text: 'bought a pie' }
+]
